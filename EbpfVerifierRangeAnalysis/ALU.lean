0ba17b28d37cd610a30b578764304169078ddbc0
Tyler Morton <tbmorton@ucsc.edu>
2025-06-08 17:52:58 -0700

    Added modified ALU

--- EbpfVerifierRangeAnalysis/ALU.lean
+++ EbpfVerifierRangeAnalysis/ALU.lean
@@ -51,15 +51,23 @@ theorem UInt32.le_nat_iff (a b : UInt32) : (a ≤ b) ↔ ((a.toNat : Nat) ≤ (b
   exact UInt32.le_iff_toNat_le
 
 theorem zero_lt_UInt32_max : (0 : UInt32) < UInt32.max := by
-  -- UInt32.max is (2^32 - 1), which is definitely greater than 0
-  -- So convert to Nat and compare:
   have h := UInt32.lt_iff_toNat_lt 0 UInt32.max
   rw [UInt32.zero] at h
   rw [UInt32.max] at h
-  -- Now show 0 < (2^32 - 1) as Nat:
   simp only [Nat.zero_lt_succ]
   exact Nat.zero_lt_pow (by decide) 32
 
+
+lemma UInt32.lt_toNat {a b : UInt32} : a < b ↔ a.toNat < b.toNat := by
+  unfold UInt32.instIntCast
+  unfold Lt.lt instLTUInt32
+  unfold LT.lt instLTUInt32
+  unfold UInt32.lt
+  simp only [compare, decide_eq_true_eq]
+  cases h : compare a b
+  case lt => simp [compare, h]
+  case eq => simp [compare, h]
+  case gt => simp [compare, h]
 
 theorem scalar_add_32_range_bounds
   (a b : BpfRegister)
@@ -69,7 +77,6 @@ theorem scalar_add_32_range_bounds
     c.u32_min < c.u32_max :=
   by
     let c := scalar_add_32 a b
-    -- unfold scalar_add_32 to get the overflow check and result bounds
     let overflow := unsignedCheckAddOverflow32 a.u32_min b.u32_min || unsignedCheckAddOverflow32 a.u32_max b.u32_max
     by_cases h_overflow : overflow
     · unfold scalar_add_32
@@ -78,9 +85,8 @@ theorem scalar_add_32_range_bounds
     · unfold scalar_add_32
       split_ifs with h
       · simp [scalar_add_32, h_overflow]
-        exact UInt32.zero_le
-
-        simp
+        rw [UInt32_max]
+      · simp [scalar_add_32, h_overflow]
         rw [UInt32.lt_iff_toNat_lt] at ha
         rw [UInt32.lt_iff_toNat_lt] at hb
         exact Nat.add_lt_add ha hb
--- EbpfVerifierRangeAnalysis/Overflow.lean
+++ EbpfVerifierRangeAnalysis/Overflow.lean
@@ -1,21 +1,26 @@
 import Mathlib.Data.Int.Init
 
+def UInt32_max : UInt32 := UInt32.ofNat (2^32 - 1)
+def UInt_max : UInt64 := UInt64.ofNat (2^64 - 1)
+def Int32_max : Int32 := Int32.maxValue
+def Int_max : Int64 := Int64.maxValue
+
 @[simp]
 def unsignedCheckAddOverflow32 (a b : UInt32) : Bool :=
   let sum := a.toNat + b.toNat
-  sum ≥ UInt32.size
+  sum ≥ UInt32_max
 
 @[simp]
 def signedCheckAddOverflow32 (a b : Int32): Bool :=
   let sum := a.toNat + b.toNat
-  sum ≥ Int32.size
+  sum ≥ Int32_max
 
 @[simp]
 def unsignedCheckAddOverflow(a b : UInt64) : Bool :=
   let sum := a.toNat + b.toNat
-  sum ≥ UInt64.size
+  sum ≥ UInt_max
 
 @[simp]
 def signedCheckAddOverflow (a b : Int64) : Bool :=
   let sum := a.toNat + b.toNat
-  sum ≥ UInt64.size
+  sum ≥ Int_max
