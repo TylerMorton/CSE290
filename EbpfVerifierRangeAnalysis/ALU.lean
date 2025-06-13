c03aff9bf52afeda27e97f4c6ff4e8ee6395ac3a
Tyler Morton <tbmorton@ucsc.edu>
2025-06-13 16:57:26 -0700

    Additional modifications

--- EbpfVerifierRangeAnalysis/ALU.lean
+++ EbpfVerifierRangeAnalysis/ALU.lean
@@ -8,9 +8,7 @@ import EbpfVerifierRangeAnalysis.Register
 
 -- **Addition**
 
-
-
-lemma aux {a b a' b' : UInt32} (h : a.toNat + b.toNat < UInt32.size) (h' : a'.toNat + b'.toNat < UInt32.size)
+lemma uint32_aux {a b a' b' : UInt32} (h : a.toNat + b.toNat < UInt32.size) (h' : a'.toNat + b'.toNat < UInt32.size)
     (hn : a.toNat + b.toNat ≤ a'.toNat + b'.toNat) :
     a + b ≤ a' + b' := by
   change (a + b).toNat ≤ (a' + b').toNat
@@ -20,34 +18,80 @@ lemma aux {a b a' b' : UInt32} (h : a.toNat + b.toNat < UInt32.size) (h' : a'.to
   · simpa using h'
   · simpa using h
 
+lemma uint64_aux {a b a' b' : UInt64} (h : a.toNat + b.toNat < UInt64.size) (h' : a'.toNat + b'.toNat < UInt64.size)
+    (hn : a.toNat + b.toNat ≤ a'.toNat + b'.toNat) :
+    a + b ≤ a' + b' := by
+  change (a + b).toNat ≤ (a' + b').toNat
+  simp only [UInt64.toNat_add]
+  rw [Nat.add_mod_of_add_mod_lt, Nat.add_mod_of_add_mod_lt]
+  · simpa using hn
+  · simpa using h'
+  · simpa using h
 
-def scalar_add_32(a b: BpfRegister) : BpfRegister :=
+
+def scalar_add_32(a b : BpfRegister) : BpfRegister :=
 -- pattern matching (have v let)
-  let res := if unsignedCheckAddOverflow32 a.u32_min b.u32_min ∨
+  let u32_res := if unsignedCheckAddOverflow32 a.u32_min b.u32_min ∨
     unsignedCheckAddOverflow32 a.u32_max b.u32_max then
     (0, UInt32_max)
     else
     (a.u32_min + b.u32_min, a.u32_max + b.u32_max)
+  let u64_res := if unsignedCheckAddOverflow a.u64_min b.u64_min ∨
+    unsignedCheckAddOverflow a.u64_max b.u64_max then
+    (0, UInt_max)
+    else
+    (a.u64_min + b.u64_min, a.u64_max + b.u64_max)
   {
-    u32_min := res.1,
-    u32_max := res.2,
-    well_formed := by
-      revert res
+    u32_min := u32_res.1,
+    u32_max := u32_res.2,
+    u32_well_formed := by
+      revert u32_res
       split_ifs
       · simp
       · simp
         rename_i h
         simp at h
-        apply aux
+        apply uint32_aux
         · simp [UInt32.size]
           omega
         · simp [UInt32.size]
           omega
-        · exact Nat.add_le_add a.well_formed b.well_formed
+        · exact Nat.add_le_add a.u32_well_formed b.u32_well_formed,
+      u64_min := a.u64_min,
+      u64_max := a.u64_max,
+      u64_well_formed := a.u64_well_formed
     var_off    := a.var_off
   }
 
-@[simp]
+def scalar_add(a b : BpfRegister) : BpfRegister :=
+-- pattern matching (have v let)
+  let u64_res := if unsignedCheckAddOverflow a.u64_min b.u64_min ∨
+    unsignedCheckAddOverflow a.u64_max b.u64_max then
+    (0, UInt_max)
+    else
+    (a.u64_min + b.u64_min, a.u64_max + b.u64_max)
+  {
+    u32_min := a.u32_min
+    u32_max := a.u32_max
+    u32_well_formed := a.u32_well_formed
+    u64_min := u64_res.1,
+    u64_max := u64_res.2,
+    u64_well_formed := by
+      revert u64_res
+      split_ifs
+      · simp
+      · simp
+        rename_i h
+        simp at h
+        apply uint64_aux
+        · simp [UInt64.size]
+          omega
+        · simp [UInt64.size]
+          omega
+        · exact Nat.add_le_add a.u64_well_formed b.u64_well_formed
+    var_off    := a.var_off
+  }
+
 theorem scalar_add_32_bounds_min (a b : BpfRegister) :
   (scalar_add_32 a b).u32_min ≤  a.u32_min + b.u32_min := by
   unfold scalar_add_32
@@ -56,7 +100,6 @@ theorem scalar_add_32_bounds_min (a b : BpfRegister) :
   · simp
   · simp
 
-@[simp]
 theorem scalar_add_32_bounds_max (a b : BpfRegister) :
   (scalar_add_32 a b).u32_max ≥  a.u32_max + b.u32_max := by
   unfold scalar_add_32
@@ -68,14 +111,29 @@ theorem scalar_add_32_bounds_max (a b : BpfRegister) :
     bv_decide
   · simp
 
+theorem scalar_add_32_bounds_well_formed (a b: BpfRegister) :
+  ((scalar_add_32 a b).u32_max ≥ (scalar_add_32 a b).u32_min) := by
+  apply BpfRegister.u32_well_formed
 
+theorem scalar_add_64_bounds_min (a b : BpfRegister) :
+  (scalar_add a b).u64_min ≤  a.u64_min + b.u64_min := by
+  unfold scalar_add
+  simp
+  split_ifs with h
+  · simp
+  · simp
 
-theorem scalar_add_32_bounds ( a b : BpfRegister) :
-  ((scalar_add_32 a b).u32_min ≤ a.u32_min + b.u32_min) ∧
-  ((scalar_add_32 a b).u32_max ≥ a.u32_max + b.u32_max) ∧
-  ((scalar_add_32 a b).u32_max ≥ (scalar_add_32 a b).u32_min)
-   := by
-  split_ands
-  · simp -- sum is le added components (modulo) (bounds_min)
-  · simp -- sum is ge added components (modulo) (bounds_max)
-  · apply BpfRegister.well_formed     -- max is always ge min
+theorem scalar_add_64_bounds_max (a b : BpfRegister) :
+  (scalar_add a b).u64_max ≥  a.u64_max + b.u64_max := by
+  unfold scalar_add
+  simp
+  split_ifs with h
+  · simp
+    unfold UInt_max
+    simp
+    bv_decide
+  · simp
+
+theorem scalar_add_bounds_well_formed (a b: BpfRegister) :
+  ((scalar_add a b).u64_max ≥ (scalar_add a b).u64_min) := by
+  apply BpfRegister.u64_well_formed
--- EbpfVerifierRangeAnalysis/Register.lean
+++ EbpfVerifierRangeAnalysis/Register.lean
@@ -5,7 +5,11 @@ structure BpfRegister where
   -- 32 bit
   u32_min : UInt32
   u32_max: UInt32
-  well_formed: u32_min ≤ u32_max
+  u32_well_formed: u32_min ≤ u32_max
+
+  u64_min: UInt64
+  u64_max: UInt64
+  u64_well_formed: u64_min ≤ u64_max
   -- s32_min : Int32
   -- s32_max: Int32
   -- 64 bit
@@ -30,21 +34,11 @@ def well_formed_min_max_uint32 (min max : UInt32) : Prop := min ≤ max
 def well_formed_min_max_int64 (min max : Int64) : Prop := min ≤ max
 def well_formed_min_max_uint64 (min max : UInt64) : Prop := min ≤ max
 
--- def well_formed_range (reg_state : BpfRegister)
---  : Prop :=
---   well_formed_min_max_int32 reg_state.s32_min reg_state.s32_max ∧
---   well_formed_min_max_uint32 reg_state.u32_min reg_state.u32_max ∧
---   well_formed_min_max_int64 reg_state.s64_min reg_state.s64_max ∧
-  -- well_formed_min_max_uint64 reg_state.u64_min reg_state.u64_max
-
 
 -- Correctness definitions
 def contains_min_max {α : Type} [LE α] (min max s : α) : Prop :=
   min ≤ s ∧ s ≤ max
 
-
-def Int64.toUInt32 (x : Int64) : UInt32 :=
-  UInt32.ofNat (x.toNat ^ (2^32))
 
 -- def contains_range(reg_state: BpfRegister) (s: Int64) : Prop :=
 --   contains_min_max reg_state.s32_min reg_state.s32_max (Int64.toInt32 s) ∧
